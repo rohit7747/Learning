5c07ebb3221917b55214e37b27027e70f165664c
rohit7747 <rsinha7747@gmail.com>
2021-09-18 15:18:41 +0530

    push of index

---
+++ index.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n , i ,j;
+		System.out.println("enter the array");
+		n = sc.nextInt();
+		int []arr = new int[n];
+		for(i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			System.out.println(arr[i]);
+			System.out.println("index no:" + i);
+        }
+    }
+}
