0c4d930d9e7e3e0e4236f4603e2db7129bf305ad
rohit7747 <rsinha7747@gmail.com>
2021-09-20 11:52:08 +0530

    left to right arra

---
+++ array_LR.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Business
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		int []arr = {4,5,6,9,0,2,11,17,5,5,4};
+		int i= 0;
+		while(i< arr.length)
+		{
+			System.out.print(" " +arr[i]);
+			i++;
+		
+		}
+		
+	
+		// your code goes here
+	}
+}
