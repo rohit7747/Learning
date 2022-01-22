ad720630a0a3ada3486f7f80fa1821c82be38614
rohit7747 <rsinha7747@gmail.com>
2022-01-22 16:11:21 +0530

    uppercase

---
+++ alphabet.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("enter the string");
+		String str = sc.nextLine();
+		System.out.println(" the string is " +str);
+		String str1 = str.toUpperCase();
+		System.out.println(" the new string is " +str1);
+		
+		
+		
+		
+		// your code goes here
+	}
+}
